aa2fe6a7190cd2bbb8467011c7f88584acdc332d
Cristian <31805252+foolfighter@users.noreply.github.com>
2021-12-13 19:52:00 +0100

    Implemented the following functionalities: *Top_up: oystercard can be topped up. The balance of the card will increase by the amount passed as argument. *MAXIMUM_BALANCE: we have a card that can be topped up but we want to limit the amount of money that can be stored there. On real Oyster cards the limit is £90, so we add it to our class too. Now, it’s impossible to top up the card beyond the limit. *Deduct: this method works as mechanism for deducting money from the oystercard. *touch_in / #touch_out / #in_journey? : card already can be topped up and can be used to spend money, so we use these methods to make it support touch in/out events by letting the card be aware whether it's being used on a journey or not.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,34 @@
-class OysterCard 
-  attr_reader :balance
-
+class Oystercard 
+  MAXIMUM_BALANCE = 90
   DEFAULT_CAPACITY = 0
 
-  def initialize(balance = DEFAULT_CAPACITY)
-    @balance = balance
+  attr_reader :balance, :travelling
+
+  def initialize
+    @balance = 0
+    @travelling = false
+  end
+
+  def top_up(amount)
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    fail "Insufficient funds" if balance - amount < 0
+    @balance -= amount
+  end
+
+  def in_journey?
+    @travelling
+  end
+
+  def touch_in
+    @travelling = true
+  end
+
+  def touch_out
+    @travelling = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,22 +1,73 @@
 # in spec/oystercard_spec.rb
 
-# In order to use public transport
-# As a customer
-# I want money on my card
-
 require 'oystercard.rb'
 
-describe OysterCard do
+describe Oystercard do
   subject(:oystercard) { described_class.new }
 
   describe "#balance" do
 
     it { is_expected.to respond_to(:balance) }
     
-    it 'has a balance of 0' do
-      expect(oystercard.balance).to eq(OysterCard::DEFAULT_CAPACITY)
+    it 'has a default balance of 0' do
+      expect(oystercard.balance).to eq(Oystercard::DEFAULT_CAPACITY)
+    end
+  
+  end  
+    
+  describe "#top_up" do
+
+    it 'can add money' do
+      expect(oystercard.top_up(10)).to eq 10
     end
 
   end
 
+
+  context 'top up limit is reached' do
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oystercard.top_up maximum_balance
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+  
+  end
+
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'raises an error if not enough balance' do
+      oystercard.top_up 10
+      expect{ subject.deduct 11 }.to raise_error "Insufficient funds"
+    end
+
+  end
+
+  describe "#in_journey?" do
+    it { is_expected.to respond_to(:in_journey?) }
+
+    it "returns if card is in journey" do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+  end
+
+  describe "#touch_in" do
+    it { is_expected.to respond_to(:touch_in) }
+
+    it "returns if card touched in" do
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to eq true
+    end
+  end
+
+  describe "#touch_out" do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it "returns if card touched in" do
+      oystercard.touch_out
+      expect(oystercard.in_journey?).to eq false
+    end
+  end
 end
